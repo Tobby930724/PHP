3489fbf61fad24ff2d885feba75075a6b3e50539
Tobby930724 <135235508+Tobby930724@users.noreply.github.com>
2024-04-12 10:06:57 +0800

    Create  1.html.php

---
+++ 1.html.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        #<!-- 在這裡插入了 PHP 代碼，用於輸出 "Hello world!" 字符串 -->
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        #<!-- 再次插入 PHP 代碼，輸出 "Hello world!" 字符串 -->
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+       #<!-- 再次插入 PHP 代碼，輸出 "Hello world!" 字符串 -->
+    </body>
+</html>
