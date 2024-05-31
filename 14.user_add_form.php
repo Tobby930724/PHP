3b8a875d23e9879049173cde0ac3d7ed77ea2c3a
Tobby930724 <135235508+Tobby930724@users.noreply.github.com>
2024-05-31 15:54:05 +0800

    Create 14.user_add_form.php

---
+++ 14.user_add_form.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>新增使用者</title>
+</head>
+<body>
+<?php        
+    // 關閉錯誤報告，以避免顯示錯誤訊息
+    error_reporting(0);
+    
+    // 啟動會話（session）
+    session_start();
+    
+    // 檢查是否有用戶登入，若未登入則顯示訊息並重新導向到登入頁面
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else {    
+        // 若已登入，顯示新增使用者的表單
+        echo "
+            <form action='15.user_add.php' method='post'>
+                帳號：<input type='text' name='id'><br>
+                密碼：<input type='text' name='pwd'><p></p>
+                <input type='submit' value='新增'> <input type='reset' value='清除'>
+            </form>
+        ";
+    }
+?>
+</body>
+</html>
